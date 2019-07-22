b12224e9082c8c9f4f30f6cb8124912e16bab275
Sostenes Andrade <sostenes.andrade@globalhitss.com.br>
2019-07-22 18:22:38 -0300

    fim

--- node_modules/@quasar/app/lib/webpack/loader.quasar-stylus-variables.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (content) {
-  return content.indexOf('$') !== -1
-    ? `@import '~src/css/quasar.variables.styl'\n@import '~quasar/src/css/variables.styl'\n` +
-      content
-    : content
-}
